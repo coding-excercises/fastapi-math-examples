01368d4b90e20897da7bc0859b769b371081c429
Rajan <rajan.manickavasagam@gmail.com>
2021-03-06 19:06:53 +0530

    fixed the test case

--- app/tests/health_test.py
+++ app/tests/health_test.py
@@ -5,7 +5,6 @@ from app.api.health_fn import health_func
 class HealthTestSuite(unittest.TestCase):
     """Test the health function."""
 
-
-    def test_func_health(self) -> None:
+    def test_func_health(self):
         result = health_func()
-        assert result == "ok"
+        self.assertEqual(result, "ok")
